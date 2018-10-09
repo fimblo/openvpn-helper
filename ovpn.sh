6e803e04f26510ca09aac9b035353e755f2f9764
Mattias Jansson <fimblo@yanson.org>
2018-10-09 14:43:02 +0200

    created getopts section, added dummy usage

--- ovpn.sh
+++ ovpn.sh
@@ -24,6 +24,21 @@
 # - option to redirect openvpn output elsewhere, with summary presented in stdout instead
 # --------------------------------------------------
 
+# --------------------------------------------------
+# Some variables
+OPENVPN_DIR=$HOME/.openvpn
+configdir=$OPENVPN_DIR/ovpn_tcp
+selected_region='default'
+
+
+# --------------------------------------------------
+# Functions
+usage () {
+  cat<<-EOF
+	blah blah
+	EOF
+  [[ "$1" ]] && echo $1
+}
 
 # simple error function
 croak () {
@@ -32,16 +47,18 @@ croak () {
 }
 
 # --------------------------------------------------
-# HELPER VARS
-OPENVPN_DIR=$HOME/.openvpn
-tcp_configdir=$OPENVPN_DIR/ovpn_tcp
-selected_region=${1:-default}
+# Catch all CLI options
 
-# --------------------------------------------------
-# sanity check
+while getopts 'h' tag ; do
+  case $tag in
+    h) usage; exit 0                          ;;
+    \?) usage "Invalid cmdline parm."; exit 1 ;;
+  esac
+done
 
-[[ ! -d $OPENVPN_DIR   ]] && croak "Cannot find config dir: $OPENVPN_DIR"          1
-[[ ! -d $tcp_configdir ]] && croak "Cannot find config dir for TCP openvpn: $tcp_configdir." 1
+[[ ! -d $OPENVPN_DIR ]] && croak "Can't find config dir: $OPENVPN_DIR"        1
+[[ ! -d $configdir   ]] && croak "Can't find openvpn config dir: $configdir." 1
+
 
 if [[ $selected_region == "default" ]] ; then
   echo "Missing cmdline arg for region (us,se,jp,etc)."
@@ -50,19 +67,20 @@ if [[ $selected_region == "default" ]] ; then
   selected_region='se'
 fi
 
+
 # --------------------------------------------------
 # check if region exists, exit if not
 tmpfile=$(mktemp) ; trap 'rm -f $tmpfile' 0
-ls -1 $tcp_configdir | cut -c-2 | sort -u > $tmpfile
+ls -1 $configdir | cut -c-2 | sort -u > $tmpfile
 if ! grep -q "$selected_region" $tmpfile ; then
   croak "Illegal region: $selected_region" 2
 fi
 
 # --------------------------------------------------
 # find all files with that region, select one at random
-config=$(find $tcp_configdir/${selected_region}* | shuf -n 1)
+config=$(find $configdir/${selected_region}* | shuf -n 1)
 
 # --------------------------------------------------
 # start openvpn with that file
-sudo openvpn $config
+echo sudo openvpn $config
 
